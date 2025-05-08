c3f0b8aa2e5e469849ac17d283d239d5ab4410a0
Fernando Palmieri <fernando.palmierione@gmail.com>
2025-05-08 18:17:42 -0300

    feat(p1): adiciona jogador construtor

--- JogoMinecraft.java
+++ JogoMinecraft.java
@@ -3,11 +3,13 @@ import java.util.Random;
 public class JogoMinecraft{
     public static void main(String[] args) throws Exception {
     var p1 = new JogadorMinecraft("Alex");
+    var p2 = new JogadorMinecraft("Steve Construtor");
     int acoes = 3;
     var gerador = new Random();
     while(true){
         var oQueFazer = 1 + gerador.nextInt(acoes); //nextdouble (if oQueFazer < 0.25)
         var verificarDano = gerador.nextDouble();
+            if (p1.estaVivo() == true){
             switch (oQueFazer){
                 case 1:
                     p1.minerar();
@@ -24,12 +26,33 @@ public class JogoMinecraft{
                 }
                 System.out.println(p1);
                 System.out.println("=====================");
-                Thread.sleep(5000);
-                if (p1.estaVivo() == false ){
-                System.out.println("GAME OVER");
-                System.out.println("=====================");
-                System.exit(1);
+                
+            }
+                if (p2.estaVivo() == true){
+                    var oQueFazerSteve = gerador.nextDouble();
+                    var verificarDanoSteve = gerador.nextDouble();
+                    if (oQueFazerSteve <=0.6){
+                        p2.construir();
+                    }
+                    if (oQueFazerSteve > 0.6 && oQueFazerSteve <= 0.9){
+                    p2.coletarMadeira();
+                    }
+                    if(oQueFazerSteve > 0.9 && oQueFazerSteve <= 1){
+                        p2.minerar();
+                    }
+                    if (verificarDanoSteve <= 0.25){
+                        p2.levarDano();
+                    }
+                    System.out.println(p2);
+                    System.out.println("=====================");
+                    
                 }
-            }        
-    }
-}
+
+                if(p1.estaVivo() == false && p2.estaVivo() == false){
+                    System.out.println("GAME OVER");
+                    break;
+                }                
+           Thread.sleep(5000);   
+        }
+    }     
+}     
